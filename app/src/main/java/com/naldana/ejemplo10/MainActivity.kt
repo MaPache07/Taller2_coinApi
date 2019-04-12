c65e2ee35e843bf78b636d5a1b6561b28955fc69
MaPache07 <00170517@uca.edu.sv>
2019-04-11 21:53:40 -0600

    Se ha agregado el modelo de la moneda y ya se obtienen los datos de la moneda

--- app/src/main/java/com/naldana/ejemplo10/MainActivity.kt
+++ app/src/main/java/com/naldana/ejemplo10/MainActivity.kt
@@ -7,8 +7,13 @@ import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import com.naldana.ejemplo10.models.Coin
 import com.naldana.ejemplo10.utilities.NetworkUtil
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
@@ -18,67 +23,63 @@ import java.net.URL
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     var twoPane =  false
-    val coinLink:  String = "https://international-coins.herokuapp.com/api/coin/"
+    val coinLink:  String = "https://apicoins.herokuapp.com/coin"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         FetchCoinTask().execute()
-        // TODO (9) Se asigna a la actividad la barra personalizada
         setSupportActionBar(toolbar)
 
-
-        // TODO (10) Click Listener para el boton flotante
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
 
-
-        // TODO (11) Permite administrar el DrawerLayout y el ActionBar
-        // TODO (11.1) Implementa las caracteristicas recomendas
-        // TODO (11.2) Un DrawerLayout (drawer_layout)
-        // TODO (11.3) Un lugar donde dibujar el indicador de apertura (la toolbar)
-        // TODO (11.4) Una String que describe el estado de apertura
-        // TODO (11.5) Una String que describe el estado cierre
         val toggle = ActionBarDrawerToggle(
             this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
         )
-
-        // TODO (12) Con el Listener Creado se asigna al  DrawerLayout
         drawer_layout.addDrawerListener(toggle)
-
-
-        // TODO(13) Se sincroniza el estado del menu con el LISTENER
         toggle.syncState()
-
-        // TODO (14) Se configura el listener del menu que aparece en la barra lateral
-        // TODO (14.1) Es necesario implementar la inteface {{@NavigationView.OnNavigationItemSelectedListener}}
         nav_view.setNavigationItemSelectedListener(this)
 
-        // TODO (20) Para saber si estamos en modo dos paneles
         if (fragment_content != null ){
             twoPane =  true
         }
-
-
-        /*
-         * TODO (Instrucciones)Luego de leer todos los comentarios añada la implementación de RecyclerViewAdapter
-         * Y la obtencion de datos para el API de Monedas
-         */
     }
 
     inner class FetchCoinTask : AsyncTask<String,Void,String>(){
         override fun doInBackground(vararg params: String?): String? {
-            var coinApi: URL = NetworkUtil().buildUrl(coinLink)
-            var coinInfo: String = NetworkUtil().getResponseFromHttpUrl(coinApi)
+            var coinApi = NetworkUtil().buildUrl(coinLink)
+            var coinParser = JsonParser()
+
+            var coinJson = NetworkUtil().getResponseFromHttpUrl(coinApi)
+            coinJson = "[" + coinJson + "]"
+            var theCoin: Coin
+            var listCoinArray = coinParser.parse(coinJson).asJsonArray
+            for(listElement: JsonElement in listCoinArray){
+                var coinObject = listElement.asJsonObject
+                var coinArray = coinObject.getAsJsonArray("coins")
+                var i:Int = 0
+                for(coinElement: JsonElement in coinArray){
+                    theCoin = Coin(coinElement.asJsonObject.get("name").asString,
+                        coinElement.asJsonObject.get("country").asString,
+                        coinElement.asJsonObject.get("value").asInt,
+                        coinElement.asJsonObject.get("value_us").asInt,
+                        coinElement.asJsonObject.get("year").asInt,
+                        coinElement.asJsonObject.get("isAvailable").asBoolean,
+                        coinElement.asJsonObject.get("img").asString)
+                    Log.d("Pooh.se.la.come", coinElement.asJsonObject.get("_id").asString)
+                    CoinList.cList.add(theCoin)
+                    CoinList.cListMap.put(coinElement.asJsonObject.get("_id").asString, theCoin)
+                    i++
+                }
+            }
+
             return null
         }
     }
 
-    // TODO (16) Para poder tener un comportamiento Predecible
-    // TODO (16.1) Cuando se presione el boton back y el menu este abierto cerralo
-    // TODO (16.2) De lo contrario hacer la accion predeterminada
     override fun onBackPressed() {
         if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
             drawer_layout.closeDrawer(GravityCompat.START)
@@ -87,14 +88,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    // TODO (17) LLena el menu que esta en la barra. El de tres puntos a la derecha
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
-    // TODO (18) Atiende el click del menu de la barra
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -105,11 +104,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    // TODO (14.2) Funcion que recibe el ID del elemento tocado
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         // Handle navigation view item clicks here.
         when (item.itemId) {
-            // TODO (14.3) Los Id solo los que estan escritos en el archivo de MENU
             R.id.nav_camera -> {
 
             }
@@ -130,8 +127,12 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
         }
 
-        // TODO (15) Cuando se da click a un opcion del menu se cierra de manera automatica
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    object CoinList{
+        var cList: ArrayList<Coin> = ArrayList()
+        var cListMap: MutableMap<String, Coin> = HashMap()
     }
 }
---
+++ app/src/main/java/com/naldana/ejemplo10/models/Coin.kt
@@ -0,0 +1,11 @@
+package com.naldana.ejemplo10.models
+
+class Coin (name: String, country: String, value: Int, values_us: Int, year: Int, isAvailable: Boolean, img: String){
+    var name: String = ""
+    var country: String = ""
+    var value: Int = 0
+    var value_us: Int = 0
+    var year: Int = 0
+    var isAvailable: Boolean = true
+    var img: String = ""
+}
